eb8f487b9abf0436530c0f991be4ce46a1790aac
Janahan10 <janahanravi10@gmail.com>
2020-10-12 19:49:48 -0400

    Finalized /links route and created fetch for static HTML

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 const host = "https://tutorial.cloudflareworkers.com"
-const url = host + "/links"
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(fetch("https://static-links-page.signalnerve.workers.dev"))
 })
 
 /**
@@ -12,24 +11,33 @@ addEventListener('fetch', event => {
  */
 async function handleRequest(request) {
 
-  if (request.url == url) {
-    var linksList = [link1, link2, link3]
-    var json = []
-  
-    for (i = 0; i < linksList.length; i++) {
-      const item = JSON.stringify(linksList[i])
-      json.push(item)
-    }
+  if (request.url == host + "/links") {
+    
+    var json = linksRequest()
 
-    return response = new Response(json, {
+    response = new Response(json, {
       headers: { 'content-type': 'application/json' },
     })
+  } else {
+
+    response = new Response("hello-workers", {
+      headers: { 'content-type': 'application/json' },
+    })
+
   }
   
+  return response
+}
 
-  return new Response("hello-workers", {
-    headers: { 'content-type': 'application/json' },
-  })
+function linksRequest() {
+  var linksList = [link1, link2, link3]
+  var links = []
+  
+  for (i = 0; i < linksList.length; i++) {
+    const item = JSON.stringify(linksList[i])
+    links.push(item)
+  }
+  return links
 }
 
 var link1 = {
